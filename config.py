d33da58d0211769168dd57f7ea5794fa6936dee8
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2023-08-22 23:41:41 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,172 @@
+"""
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇Rocket League Quick Chat⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⡿⠋⠉⠈⠈⠈⠙⠻⠿⠻⠛⠛⠋⠋⠉⠉⠉⠉⠉⠉⠉⠋⠛⠛⠛⠟⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⠏⠀⣰⣾⣿⣿⣿⣦⡄⠀⢤⣴⣶⣶⣶⣷⣷⣷⣷⣷⣷⣷⣶⣶⣶⣴⣤⣄⣀⡀⠈⠉⠛⠿⣿⣿⣿⣿
+⣿⡏⠀⢼⣿⣿⣿⣿⣿⣿⣿⡀⠨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣀⠀⠙⢻⣿
+⣿⣧⠀⢹⣿⣿⣿⣿⣿⣿⣿⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⠀⢸⣿
+⣿⣿⡄⠀⠛⢿⣿⣿⣿⠿⠁⢀⡾⢿⣿⣿⣿⣿⠿⡿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⢸⣿
+⣿⣿⡆⠀⣄⣀⢀⢀⢀⣠⢬⣺⠈⠈⠈⠈⢘⢙⡛⣢⣶⣤⣥⢀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⠀⢸⣿
+⣿⣿⣧⠀⢸⣿⣿⣿⣿⣿⡉⢪⢊⣦⣀⠶⠒⠃⢄⠉⢯⡭⠭⠈⠓⠈⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣽⣿
+⣿⣿⣿⠄⠈⣿⣿⣿⣿⣿⣷⣤⣭⣵⡄⠀⠀⠖⣄⢩⡀⠀⠀⠀⣬⣀⠀⡤⠝⠿⠿⣿⣿⣿⣿⣿⠀⢠⣿⣿⡇
+⣿⣿⣿⣧⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠩⠆⢃⣦⣤⣤⡐⠋⠔⠀⢀⣈⢻⣿⣦⣄⠙⢿⣿⠇⠀⣾⣿⣿
+⣿⣿⣿⣿⡆⠀⠈⠛⠿⢿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿⡆⠀⠢⣂⠻⠿⡿⡿⣧⠀⠹⠀⣸⣿⣿⣿
+⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠉⠉⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣶⣤⣤⣤⡀⠀⠀⢠⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⡄⠀⢴⣾⣶⣶⣦⣦⣤⣀⡀⡀⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⠀⢠⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡄⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⣠⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡄⠻⣿⣿⣿⣿⣿⠏⠀⣴⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠈⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡘⢿⣿⠟⠁⢀⣾⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠹⠉⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡀⠈⠻⢿⣿⣿⣿⣿⣿⠿⠋⠁⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⡀⠈⠙⠟⠋⠁⣀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+
+Here you can customize messages and rebind keys
+"""
+
+# The full variety of text messaging begins below
+# 1_1, 1_2 etc. is the order in which the keys are pressed
+quick_chat_1_1 = [
+    "Dear mate, let me take that kickoff!"
+    ]
+
+quick_chat_1_2 = [
+    "Please mate, take that shot!"
+    ]
+
+quick_chat_1_3 = [
+    "I'm here!"
+    ]
+
+quick_chat_1_4 = [
+    "I will defend for you!"
+    ]
+
+quick_chat_2_1 = [
+    "Dddaaamn! That shot!",
+    "Did you program the AI to shot like that?",
+    "Remember, hitting the ball is optional.",
+    "Nice shot! If you were aiming for the bleachers.",
+    "Almost had it! Don't worry, I'm sure you'll score one day."
+    ]
+
+quick_chat_2_2 = [
+    "Incredible pass!",
+    "That pass was so good, I almost thought it was an accident."
+    ]
+
+quick_chat_2_3 =[
+    "Thanks a lot!"
+    ]
+
+quick_chat_2_4 = [
+    "Here's the code for practicing saves: 2E68-0A19-F54F-D41F",
+    "You'd better not try to save it!",
+    "A child of 3 yo can save that. You not tho :)",
+    "My Lord, who am I playing with?",
+    "This is the worst save ever!"
+    ]
+
+quick_chat_3_1 = [
+    "That one was close enough"
+    ]
+
+quick_chat_3_2 = [
+    "NIEN! NIEN! NIEN!"
+    ]
+
+quick_chat_3_3 = [
+    "Holy Wow!"
+    ]
+
+quick_chat_3_4 = [
+    "I was calculating that for years!",
+    "Someone get me a calculator, quick!"
+    ]
+
+quick_chat_4_1 = [
+    "Okay..."
+    ]
+
+quick_chat_4_2 = [
+    "No problema, noob."
+    ]
+
+quick_chat_4_3 = [
+    "Oops, that is your mistake."
+    ]
+
+quick_chat_4_4 = [
+    "Sorry, I'm not sorry.",
+    "My bad, I was practicing my 'spectator mode' skills.",
+    "Sorry, I was too busy being entertained by your gameplay."
+    ]
+
+
+quick_chat_messages = [
+        [
+            quick_chat_1_1,
+            quick_chat_1_2,
+            quick_chat_1_3,
+            quick_chat_1_4
+            ],
+        [
+            quick_chat_2_1,
+            quick_chat_2_2,
+            quick_chat_2_3,
+            quick_chat_2_4
+            ],
+        [
+            quick_chat_3_1,
+            quick_chat_3_2,
+            quick_chat_3_3,
+            quick_chat_3_4
+            ],
+        [
+            quick_chat_4_1,
+            quick_chat_4_2,
+            quick_chat_4_3,
+            quick_chat_4_4
+            ]
+        ]
+
+# Values set due that table:
+# https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+key_bindings = {
+        'RLAC_START': 0x70,
+        'RLAC_END': 0x71,
+        'TEXT_CHAT_ALL': 0x54,
+        'TEXT_CHAT_PARTY': 0x59,
+        'INFORMATION(TEAM)': 0x31,
+        'COMPLIMENTS': 0x32,
+        'REACTIONS': 0x33,
+        'APOLOGIES': 0x34,
+        'SHIFT': 0x10,
+        'ENTER': 0x0D
+        }
+
+# Symbols that need to be printed with shift pressed
+shift_symbols = {
+        '!','@','#','$','%','^','&','*','(',')',
+        '{','}','"',':','_','+','<','>','?','~'
+        }
+
+# 
+active_RL_keyboard_keys = {
+        'DRIVE_FORWARD': 0x57,
+        'DRIVE_BACKWARDS': 0x53,
+        'STEER_RIGHT': 0x44,
+        'STEER_LEFT': 0x41,
+        'POWERSLIDE': 0xA0,
+        'AIR_ROLL': 0xA0,
+        'FOCUS_ON_BALL': 0x20,
+        'AIR_ROLL_RIGHT': 0x45,
+        'AIR_ROLL_LEFT': 0x51,
+        'SCOREBOARD': 0x09,
+        'VOICE_CHAT': 0x46,
+        }
+
+KEYEVENTF_KEYUP = 0x0002
+
+WAIT_TIME_SECOND_CLICK = 2.5
