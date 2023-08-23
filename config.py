215604280324bfcc0fcadd2d1428b3877b51b4ad
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2023-08-23 17:53:02 +0500

    Fixed logic + new messages

--- config.py
+++ config.py
@@ -22,118 +22,180 @@
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⡀⠈⠙⠟⠋⠁⣀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
 
-Here you can customize messages and rebind keys
+Welcome to config file!
+Here you can customize messages and rebind keys.
+
 """
+if __name__ == "__main__":
 
-# The full variety of text messaging begins below
-# 1_1, 1_2 etc. is the order in which the keys are pressed
-quick_chat_1_1 = [
-    "Dear mate, let me take that kickoff!"
-    ]
+    print("It's a config file. It is only used to store variables.")
 
-quick_chat_1_2 = [
-    "Please mate, take that shot!"
-    ]
+else:
 
-quick_chat_1_3 = [
-    "I'm here!"
-    ]
-
-quick_chat_1_4 = [
-    "I will defend for you!"
-    ]
-
-quick_chat_2_1 = [
-    "Dddaaamn! That shot!",
-    "Did you program the AI to shot like that?",
-    "Remember, hitting the ball is optional.",
-    "Nice shot! If you were aiming for the bleachers.",
-    "Almost had it! Don't worry, I'm sure you'll score one day."
-    ]
-
-quick_chat_2_2 = [
-    "Incredible pass!",
-    "That pass was so good, I almost thought it was an accident."
-    ]
-
-quick_chat_2_3 =[
-    "Thanks a lot!"
-    ]
-
-quick_chat_2_4 = [
-    "Here's the code for practicing saves: 2E68-0A19-F54F-D41F",
-    "You'd better not try to save it!",
-    "A child of 3 yo can save that. You not tho :)",
-    "My Lord, who am I playing with?",
-    "This is the worst save ever!"
-    ]
-
-quick_chat_3_1 = [
-    "That one was close enough"
-    ]
-
-quick_chat_3_2 = [
-    "NIEN! NIEN! NIEN!"
-    ]
-
-quick_chat_3_3 = [
-    "Holy Wow!"
-    ]
-
-quick_chat_3_4 = [
-    "I was calculating that for years!",
-    "Someone get me a calculator, quick!"
-    ]
-
-quick_chat_4_1 = [
-    "Okay..."
-    ]
-
-quick_chat_4_2 = [
-    "No problema, noob."
-    ]
-
-quick_chat_4_3 = [
-    "Oops, that is your mistake."
-    ]
-
-quick_chat_4_4 = [
-    "Sorry, I'm not sorry.",
-    "My bad, I was practicing my 'spectator mode' skills.",
-    "Sorry, I was too busy being entertained by your gameplay."
-    ]
-
-
-quick_chat_messages = [
-        [
-            quick_chat_1_1,
-            quick_chat_1_2,
-            quick_chat_1_3,
-            quick_chat_1_4
-            ],
-        [
-            quick_chat_2_1,
-            quick_chat_2_2,
-            quick_chat_2_3,
-            quick_chat_2_4
-            ],
-        [
-            quick_chat_3_1,
-            quick_chat_3_2,
-            quick_chat_3_3,
-            quick_chat_3_4
-            ],
-        [
-            quick_chat_4_1,
-            quick_chat_4_2,
-            quick_chat_4_3,
-            quick_chat_4_4
-            ]
+    # The full variety of text messaging begins below
+    # 1_1, 1_2 etc. is the order in which the keys are pressed
+    quick_chat_1_1 = [
+        "Dear mate, let me take that kickoff!",
+        "I'll take that, no worries.",
+        "Look how pros make kickoffs."
         ]
 
-# Values set due that table:
-# https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-key_bindings = {
+    quick_chat_1_2 = [
+        "Please mate, take that shot!",
+        "Ooh, I have no hands to take that :("
+        ]
+
+    quick_chat_1_3 = [
+        "I'm here!",
+        "I'm waiting for your pass!",
+        "Mb some pass play?"
+        ]
+
+    quick_chat_1_4 = [
+        "I will defend for you!",
+        "It's better to stay in defence!",
+        "Defender of the year, right here!"
+        ]
+
+# Mind that msg with codes should be first in the list
+    quick_chat_2_1 = [
+        "Oh, God, I hope this will help: ",
+        "Mind blowing shot!",
+        "Did you program the AI to shot like that?",
+        "Nice shot! If you were aiming for the bleachers!",
+        "Almost had it! Don't worry, I'm sure you'll score one day!"
+        ]
+
+    quick_chat_2_2 = [
+        "Incredible pass!",
+        "That pass was so good, I almost thought it was an accident.",
+        "Pass of the century, right?",
+        "Pass level: Expert!"
+        ]
+
+    quick_chat_2_3 =[
+        "Thanks a lot!",
+        "Danke, danke",
+        "Shh, shh, shh, shh.",
+        "Much appreciated!"
+        ]
+
+# Mind that msg with codes should be first in the list
+    quick_chat_2_4 = [
+        "Here's the code for practicing saves: ",
+        "You'd better not try to save it!",
+        "A child of 3 yo can save that. You not tho :)",
+        "My Lord, who am I playing with?",
+        "This is the worst save ever!",
+        "Remember, hitting the ball is optional.",
+        "I'll upload that on Youtube XD"
+        ]
+
+    quick_chat_3_1 = [
+        "That one was close enough",
+        "Phew, that was a near miss!",
+        "Narrowly escaped that one!",
+        "Wow, that was cutting it close!",
+        "Almost didn't make it, huh?",
+        "Heart skipping a beat there, huh?",
+        "You're really keeping us on the edge!"
+        ]
+
+    quick_chat_3_2 = [
+        "NIEN! NIEN! NIEN!",
+        "Nope.",
+        "NOOOOO!"
+        ]
+
+    quick_chat_3_3 = [
+        "Holy Wow!",
+        "Ain't no cap bro!",
+        "You gotta be kidding!"
+        ]
+
+    quick_chat_3_4 = [
+        "I was calculating that for years!",
+        "Someone get me a calculator, quick!",
+        "It's going exactly as I planned it!"
+        ]
+
+    quick_chat_4_1 = [
+        "Okay...",
+        "Let it be...",
+        "Bruuuh.",
+        "Alright, if you say so..."
+        ]
+
+    quick_chat_4_2 = [
+        "No problema, noob!",
+        "One more apology and maybe I'll forgive you.",
+        "The 'sorry' symphony continues.",
+        "We'll pretend we didn't notice, okay?"
+        ]
+
+    quick_chat_4_3 = [
+        "Oops, that is your mistake!",
+        "D'oh.",
+        "Oh, butterfingers!",
+        "Whoops, my hands were busy observing your mom."
+        ]
+
+    quick_chat_4_4 = [
+        "Sorry, I'm not sorry.",
+        "My bad, I was practicing my 'spectator mode' skills.",
+        "Sorry, I was too busy being entertained by your gameplay."
+        ]
+
+    # Quick chat messages
+    quick_chat_messages = [
+            [
+                quick_chat_1_1,
+                quick_chat_1_2,
+                quick_chat_1_3,
+                quick_chat_1_4
+                ],
+            [
+                quick_chat_2_1,
+                quick_chat_2_2,
+                quick_chat_2_3,
+                quick_chat_2_4
+                ],
+            [
+                quick_chat_3_1,
+                quick_chat_3_2,
+                quick_chat_3_3,
+                quick_chat_3_4
+                ],
+            [
+                quick_chat_4_1,
+                quick_chat_4_2,
+                quick_chat_4_3,
+                quick_chat_4_4
+                ]
+            ]
+
+    # List of map codes that you would recommend practicing to your team/opponents
+    shooting_trainig_map_codes = [
+        "EC70-54C0-9928-E6BC",
+        "42BF-686D-E047-574B",
+        "2D89-9321-42D2-48BA",
+        "79B1-C81A-521A-B98D",
+        "5162-B381-222C-1B07",
+        "E146-BD21-EBE3-8D47"
+        ]
+
+    defence_trainig_map_codes = [
+        "2E68-0A19-F54F-D41F",
+        "5CCE-FB29-7B05-A0B1",
+        "3420-F216-ED7C-8011",
+        "2E23-ABD5-20C6-DBD4",
+        "5CB2-6D82-1B54-47B7",
+        "99E5-4AA3-60D5-44BD"
+        ]
+
+    # Values set due that table:
+    # https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+    key_bindings = {
         'RLAC_START': 0x70,
         'RLAC_END': 0x71,
         'TEXT_CHAT_ALL': 0x54,
@@ -146,14 +208,14 @@ key_bindings = {
         'ENTER': 0x0D
         }
 
-# Symbols that need to be printed with shift pressed
-shift_symbols = {
+    # Symbols that need to be printed with shift pressed
+    shift_symbols = {
         '!','@','#','$','%','^','&','*','(',')',
         '{','}','"',':','_','+','<','>','?','~'
         }
 
-# 
-active_RL_keyboard_keys = {
+    # *In the development of the last keystrokes*
+    active_RL_keyboard_keys = {
         'DRIVE_FORWARD': 0x57,
         'DRIVE_BACKWARDS': 0x53,
         'STEER_RIGHT': 0x44,
@@ -167,6 +229,8 @@ active_RL_keyboard_keys = {
         'VOICE_CHAT': 0x46,
         }
 
-KEYEVENTF_KEYUP = 0x0002
+    KEYEVENTF_KEYUP = 0x0002
 
-WAIT_TIME_SECOND_CLICK = 2.5
+    # During this time you have to press playback,
+    #   otherwise the first press will be reset
+    WAIT_TIME_SECOND_CLICK = 2.5
